999dd5617d18742f334c42436db587a7f6966228
yukimi.y <yyy.yukimi@gmail.com>
2020-12-01 14:34:36 +0900

    Q10を修正

--- task.rb
+++ task.rb
@@ -92,8 +92,7 @@ def q10
 
   # 以下に回答を記載
   foods.each do |food|
-    message = food.include?("うに") 
-    if message == true
+    if food.include?("うに")
       puts "好物です"
     else
       puts "まぁまぁ好きです"
